509609a0a81750d451bf7e1ef7a18de79a7d16ba
yur_chu <poninka@ukr.net>
2025-01-16 21:08:29 +0200

    hw-4-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
-function slugify(title) {
-  const strLowerCase = title.toLowerCase();
-  const arrLowerCase = strLowerCase.split(' ');
-  return arrLowerCase.join('-');
+function isEnoughCapacity(products, containerSize) {
+  const values = Object.values(products);
+  let totalProducts = 0;
+  for (let i = 0; i < values.length; i++) {
+    totalProducts += values[i];
+  }
+  return totalProducts <= containerSize ? true : false;
 }
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
